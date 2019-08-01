9c9db79f9e05cfb8ac52da276a07d8860d8d742c
daokuang <434346649@qq.com>
2019-08-01 21:06:57 +0800

    解决 老刘的 bug 新增模板下载

--- src/main/java/com/btjf/controller/ExcelController.java
+++ src/main/java/com/btjf/controller/ExcelController.java
@@ -4,6 +4,7 @@ import com.btjf.application.util.XaResult;
 import com.btjf.controller.base.ProductBaseController;
 import com.btjf.excel.ExcelHandlerHelper;
 import com.wordnik.swagger.annotations.Api;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +44,35 @@ public class ExcelController extends ProductBaseController{
     }
 
 
+    /**
+     * 下载模板文件
+     *
+     * @return
+     */
+    @RequestMapping(value = "/download", method = RequestMethod.POST)
+    public XaResult<String> importExcel(Integer fileType) throws Exception {
+        if(fileType == null){
+            return XaResult.error("数据类型为空");
+        }
+        String filename = null;
+        switch (fileType){
+            case 1:
+                filename = "批量新增仓库材料.xlsx";
+                break;
+            case 2:
+                filename = "仓库材料批量入库.xlsx";
+                break;
+            case 3:
+                filename = "批量新增型号耗料.xlsx";
+                break;
+        }
+        if (StringUtils.isEmpty(filename)){
+            return XaResult.error("模板不存在");
+        }
+        String url = "/download/" + filename;
+        return XaResult.success(url);
 
+    }
 
 
 }
