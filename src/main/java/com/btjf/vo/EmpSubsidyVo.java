bddfee5fe01997d1cb42c27fab290ef352a640d3
liuyqq <liuyq@cheok.com>
2019-09-25 21:54:58 +0800

    住房补贴列表 及导出

--- src/main/java/com/btjf/vo/EmpSubsidyVo.java
+++ src/main/java/com/btjf/vo/EmpSubsidyVo.java
@@ -1,5 +1,7 @@
 package com.btjf.vo;
 
+import com.btjf.model.emp.EmpSalaryMonthly;
+
 public class EmpSubsidyVo {
 
     private Integer id;
@@ -7,6 +9,17 @@ public class EmpSubsidyVo {
     private String dept;
     private String work;
     private Double subsidy;
+
+    public EmpSubsidyVo() {
+    }
+
+    public EmpSubsidyVo(EmpSalaryMonthly t) {
+        this.id = t.getId();
+        this.name = t.getEmpName();
+        this.dept = t.getDeptName();
+        this.work = t.getWorkName();
+        this.subsidy = t.getScore() == null ? 0 : t.getScore().doubleValue();
+    }
 
     public Integer getId() {
         return id;
