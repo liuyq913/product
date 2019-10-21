bb3f9ac0562167d1ae62bfd100159095b9e07b05
liuyq913 <2273852279@qq.com>
2019-10-21 21:56:36 +0800

    分罗

--- src/main/java/com/btjf/service/order/ProductionOrderService.java
+++ src/main/java/com/btjf/service/order/ProductionOrderService.java
@@ -108,8 +108,8 @@ public class ProductionOrderService {
         Integer assignNum = productionOrder.getAssignNum();
         Integer sort = 0;
         if (null != productionOrder && productionOrder.getIsLuo() == 1) {
+            if (assignNum % productionOrder.getLuoNum() > 1) throw new BusinessException("分萝错误，请修改");
             do {
-                if (assignNum % productionOrder.getLuoNum() > 1) throw new BusinessException("分萝错误，请修改");
                 ProductionLuo productionLuo = new ProductionLuo();
                 productionLuo.setIsDelete(0);
                 productionLuo.setOrderNo(productionOrder.getOrderNo());
